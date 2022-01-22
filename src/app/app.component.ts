05bd9e59d7c1fe62242ef3d3adfc40a1c220cba5
Md. A. Razzak <md.a.razzak@appstick.com.bd>
2022-01-22 18:41:48 +0600

    Data grid init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {sampleData} from "./dataSource";
+import {MenuEventArgs} from '@syncfusion/ej2-navigations';
+import {TreeGridComponent} from "@syncfusion/ej2-angular-treegrid";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'data-grid-example';
+export class AppComponent implements OnInit {
+  public data: Object[] | undefined;
+  public pager: Object | undefined;
+  public editSettings: Object | undefined;
+  public contextMenuItems: Object[] | undefined;
+  public multiSort: boolean = false;
+  public frozen: number = 0;
+
+  @ViewChild('treeGrid')
+  public treeGridObj: TreeGridComponent | undefined;
+
+  ngOnInit(): void {
+    this.data = sampleData;
+    this.editSettings = {allowEditing: true, allowAdding: true, allowDeleting: true, mode: "Row"};
+    this.updateMenu()
+  }
+
+  updateMenu(): void {
+    this.contextMenuItems = [
+      {text: 'Edit Column', target: '.col-full-menu.e-headercell', id: 'edit-column', iconCss: "col-menu-right-icon"},
+      {text: 'New Column', target: '.e-headercell', id: 'new-column', iconCss: "col-menu-right-icon"},
+      {text: 'Delete Column', target: '.col-full-menu.e-headercell', id: 'del-column', iconCss: "col-menu-right-icon"},
+      {text: 'Freeze Column', target: '.e-headercell', id: 'freeze-column', iconCss: 'far fa-check-square col-menu-right-icon'},
+      {text: 'Filter Column', target: '.e-headercell', id: 'filter-column', iconCss: "far fa-check-square col-menu-right-icon"},
+      {text: 'Multi Sort', target: '.e-headercell', id: 'multi-sort', iconCss: `${this.multiSort ? 'far fa-check-square' : 'far fa-square'} col-menu-right-icon`},
+      {text: 'Collapse the Row', target: '.e-content', id: 'collapserow', iconCss: "col-menu-right-icon"},
+    ]
+  }
+
+  contextMenuClick(args?: MenuEventArgs): void {
+    console.log(args)
+    if (args!.item.id === 'multi-sort') {
+      this.multiSort = !this.multiSort
+      this.updateMenu()
+    } else if (args!.item.id === 'freeze-column') {
+      // @ts-ignore
+      this.frozen = args!.column.index
+    }
+    // if (args!.item.id === 'collapserow') {
+    //   this.treeGridObj!.collapseRow(<HTMLTableRowElement>(this.treeGridObj!.getSelectedRows()[0]));
+    // } else {
+    //   this.treeGridObj!.expandRow(<HTMLTableRowElement>(this.treeGridObj!.getSelectedRows()[0]));
+    // }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule,ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import { PageService, FilterService,EditService,ToolbarService } from '@syncfusion/ej2-angular-treegrid';
+import { SortService, ResizeService, ExcelExportService, PdfExportService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
+import {ButtonModule} from '@syncfusion/ej2-angular-buttons';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 
+/**
+ * Module
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeGridModule,
+    ButtonModule,
+    DropDownListAllModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [PageService,
+    SortService,
+    FilterService,
+    EditService,
+    SortService, ResizeService,
+    ExcelExportService,
+    PdfExportService, ContextMenuService,
+    ToolbarService]
 })
 export class AppModule { }
